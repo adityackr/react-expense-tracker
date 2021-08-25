525f84f3a5343556f91dd8ac928d7e0a5ee67649
Aditya Chakraborty <78040114+adityackr@users.noreply.github.com>
2021-08-25 12:39:00 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Expenses from './components/Expenses/Expenses';
+import NewExpnese from './components/NewExpense/NewExpnese';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const DUMMY_EXPENSES = [
+        {
+            id: 'e1',
+            title: 'Toilet Paper',
+            amount: 50,
+            date: new Date(2020, 7, 14),
+        },
+        {
+            id: 'e2',
+            title: 'New TV',
+            amount: 25000,
+            date: new Date(2021, 2, 12),
+        },
+        {
+            id: 'e3',
+            title: 'Car Insurance',
+            amount: 15000,
+            date: new Date(2021, 2, 28),
+        },
+        {
+            id: 'e4',
+            title: 'New Desk (Wooden)',
+            amount: 5000,
+            date: new Date(2021, 5, 12),
+        },
+    ];
+
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+    const addExpenseDataHandler = (expense) => {
+        setExpenses((prevExpenses) => {
+            return [expense, ...prevExpenses];
+        });
+    };
+
+    return (
+        <div>
+            <NewExpnese onAddExpense={addExpenseDataHandler} />
+            <Expenses items={expenses} />
+        </div>
+    );
+};
 
 export default App;
---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,23 @@
+import Card from '../UI/Card';
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+const ExpenseItem = (props) => {
+    return (
+        <li>
+            <Card className="expense-item">
+                <ExpenseDate date={props.date} />
+                <div className="expense-item__description">
+                    <h2>{props.title}</h2>
+                    <div className="expense-item__price">
+                        ৳
+                        {Number(
+                            parseFloat(props.amount).toFixed(2)
+                        ).toLocaleString('en')}
+                    </div>
+                </div>
+            </Card>
+        </li>
+    );
+};
+
+export default ExpenseItem;
